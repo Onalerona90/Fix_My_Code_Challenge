86390c11a385be4e8579717053d665d98f414f92
Onalerona Pako <onapako90@gmail.com>
2023-11-04 00:40:54 +0200

    Initial commit

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -15,26 +15,25 @@ def fizzbuzz(n):
     if n < 1:
         return
 
+    tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3 == 0) and (i % 5 == 0):
-            print("FizzBuzz", end=' ')
-        elif (i % 3 == 0):
-            print("Fizz", end=' ')
-        elif (i % 5 == 0):
-            print("Buzz", end=' ')
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 3) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
         else:
-            print(i, end=' ')
-    print()
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) <= 1:
+        print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    try:
-        number = int(sys.argv[1])
-        fizzbuzz(number)
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
-        sys.exit(1)
+    number = int(sys.argv[1])
+    fizzbuzz(number)
