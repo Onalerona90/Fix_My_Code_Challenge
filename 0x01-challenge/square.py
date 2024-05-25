05b799584d2566069b5f6146f2b8236fc67f2eb2
Onalerona Pako <onapako90@gmail.com>
2024-05-25 02:11:38 +0200

    Fix code

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+class square():
+    """ A class representing a square. """
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize a Square object with optional width and height. """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
